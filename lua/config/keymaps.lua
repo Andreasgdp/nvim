c2144e6370c467f142f71f546871c11a51f752c9
Andreas Petersen <andreasgdp@gmail.com>
2023-05-29 15:28:34 +0200

    Add links to keymap references to more easily add them in the future

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Old remaps are here: https://github.com/Andreasgdp/nvim/blob/6992433392cf6d9c5612de385d8e83d6487a9750/lua/theprimeagen/remap.lua
+-- Custom LazyVim editor keymaps are here: https://github.com/LazyVim/LazyVim/blob/6b68378c2c5a6d18b1b4c5ca4c71441997921200/lua/lazyvim/plugins/editor.lua#L95
 -- Add any additional keymaps here
 local Util = require("lazyvim.util")
 
