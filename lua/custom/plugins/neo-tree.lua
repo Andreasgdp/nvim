c32fb76dd182a46b63ea418b95bb0cc5a17622a4
Andreas Petersen <andreasgdp@gmail.com>
2024-03-04 20:33:57 +0100

    Revert "chore: remove default show hidden files as won't use"
    This reverts commit 1d174282dbc7c32ec80191daed1f57f2b740648e.

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -20,6 +20,14 @@ return {
       window = {
         position = 'right',
       },
+      -- TODO: Only use this once Warp terminal has fixed issue with hiding all text for show hidden in neo-tree
+      -- filesystem = {
+      --   filtered_items = {
+      --     visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      --     hide_dotfiles = false,
+      --     hide_gitignored = true,
+      --   },
+      -- },
       event_handlers = {
 
         {
