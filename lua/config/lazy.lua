448b04e7cebf39c710d1fd1ad97fc75423a833fc
Andreas Petersen <andreasgdp@gmail.com>
2024-03-05 19:26:55 +0100

    Improve yank and put functionalities with yanky

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
 
     -- import/override with your plugins
     { import = "plugins" },
