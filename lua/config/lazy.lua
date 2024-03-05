8a2fdbaa1cbae8c761f102876fd197c8b5fb3184
Andreas Petersen <andreasgdp@gmail.com>
2024-03-05 19:00:12 +0100

    Add breadcrumbs to lualine

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,11 +10,14 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.editor.navic" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
