dc0315caabb0c3478c3117a52a63bc57de820fd0
Andreas Petersen <andreasgdp@gmail.com>
2024-03-06 23:12:40 +0100

    Upgrade git setup with better gitsigns and addition of git fugitive

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- When opening the first and only the first file, run :Gitsigns toggle_current_line_blame<CR>
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+  once = true,
+  callback = function()
+    -- vim.cmd("Gitsigns toggle_current_line_blame")
+    vim.defer_fn(function()
+      vim.cmd("Gitsigns toggle_current_line_blame")
+    end, 100)
+  end,
+})
