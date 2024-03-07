95da80f8437f484570990109abb0a5c949f89356
Andreas Petersen <andreasgdp@gmail.com>
2024-03-07 13:01:27 +0100

    Faster keybinds for git-stuff

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -6,12 +6,22 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns preview_hunk_inline<CR>", {})
-      vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gsh", ":Gitsigns stage_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>grh", ":Gitsigns reset_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gsb", ":Gitsigns stage_buffer<CR>", {})
-      vim.keymap.set("n", "<leader>grb", ":Gitsigns reset_buffer<CR>", {})
+      vim.keymap.set("n", "<leader>gd", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk" })
+      vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<CR>", { desc = "Undo last stage" })
+      vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage current hunk" })
+      vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset current hunk" })
+      vim.keymap.set(
+        "n",
+        "<leader>gbs",
+        ":Gitsigns stage_buffer<CR>",
+        { desc = "Stage buffer i.e. selected open file" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>gbr",
+        ":Gitsigns reset_buffer<CR>",
+        { desc = "Reset buffer i.e. selected open file" }
+      )
     end,
   },
 }
