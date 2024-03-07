a33b4538c62854ab149f989a9615d8356c6eca90
Andreas Petersen <andreasgdp@gmail.com>
2024-03-07 09:32:56 +0100

    More minimal keybinds for gitsigns

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -6,9 +6,7 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
-      vim.keymap.set("n", "<leader>gtb", ":Gitsigns toggle_current_line_blame<CR>", {})
-      vim.keymap.set("n", "<leader>gtd", ":Gitsigns toggle_deleted<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns preview_hunk_inline<CR>", {})
       vim.keymap.set("n", "<leader>ghs", ":Gitsigns stage_hunk<CR>", {})
       vim.keymap.set("n", "<leader>ghu", ":Gitsigns undo_stage_hunk<CR>", {})
       vim.keymap.set("n", "<leader>ghr", ":Gitsigns reset_hunk<CR>", {})
@@ -17,3 +15,5 @@ return {
     end,
   },
 }
+
+--
