41521e455563337a90439134598cc04b0f36b972
Andreas Petersen <andreasgdp@gmail.com>
2024-03-15 19:25:53 +0100

    Remove unused git keymaps and move to keymaps.lua

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -4,23 +4,9 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gd", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk" })
-      vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset current hunk" })
-      vim.keymap.set(
-        "n",
-        "<leader>gbs",
-        ":Gitsigns stage_buffer<CR>",
-        { desc = "Stage buffer i.e. selected open file" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>gbr",
-        ":Gitsigns reset_buffer<CR>",
-        { desc = "Reset buffer i.e. selected open file" }
-      )
-    end,
+    -- config = function()
+    --   require("gitsigns").setup()
+    -- end,
   },
   {
     "NeogitOrg/neogit",
