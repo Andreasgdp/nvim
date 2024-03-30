4e7c8f02b01e06c3176b6342242bab87525b6c05
Andreas Petersen <andreasgdp@gmail.com>
2024-03-30 21:10:16 +0100

    Add ability to telescope search for hidden files
    This has specific files excluded from the search e.g. node_modules, .git, etc.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,5 +4,13 @@ return {
     defaults = {
       wrap_results = true,
     },
+    pickers = {
+      live_grep = {
+        file_ignore_patterns = { "node_modules", ".git", ".venv", ".env", ".cache" },
+        additional_args = function(_)
+          return { "--hidden" }
+        end,
+      },
+    },
   },
 }
