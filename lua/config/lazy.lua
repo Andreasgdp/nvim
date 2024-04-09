45e92cf5c0fe0bb2c0b70300598a03f3e48e2870
Andreas Petersen <andreasgdp@gmail.com>
2024-04-09 13:42:25 +0200

    Cleanup of plugins and stuff I don't use

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,8 +16,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.navic" },
-    { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.java" },
