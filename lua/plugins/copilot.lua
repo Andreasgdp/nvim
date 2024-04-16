215a27bffad8437e366597ad6a1958a9d58c584f
Andreas Petersen <andreasgdp@gmail.com>
2024-04-16 17:48:36 +0200

    Remove debounce for faster suggestions from copilot when using it

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -7,7 +7,7 @@ return {
       suggestion = {
         enabled = true,
         auto_trigger = true,
-        debounce = 75,
+        debounce = 0,
         keymap = {
           accept = "<C-J>",
           accept_word = false,
