e4970294dcdc90018794af38492bc0c52536b930
Andreas Petersen <andreasgdp@gmail.com>
2024-04-29 14:25:36 +0200

    Add toggle for search and replace

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -5,6 +5,7 @@ return {
   opts = { open_cmd = "noswapfile vnew" },
   -- stylua: ignore
   keys = {
+    { "<leader>S", function() require("spectre").toggle() end, desc = "Toggle Replace (spectre)" },
     { "<leader>sR", function() require("spectre").open() end, desc = "Replace in Files (Spectre)" },
     { "<leader>sr", function() require("spectre").open_file_search({select_word=true}) end, desc = "Search on current file" , remap = true },
   },
