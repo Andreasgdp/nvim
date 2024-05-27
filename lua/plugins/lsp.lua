d7088beb7908095aa58befbdb751b7eb954f6dc8
Andreas Petersen <andreasgdp@gmail.com>
2024-05-27 17:58:36 +0200

    Add todo for setting up basic lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,4 @@
+-- TODO: Add basic LSP support and ensure languages I use have LSP support. 
+-- Basically it is only to ensure lsp and mason have the language servers installed
+-- The rest is handled by the setup provided by LazyVim
+return {}
